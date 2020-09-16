096be63a2212f9850629245469120249319f8341
ckwam <ckwa@protonmail.com>
2020-09-17 02:15:04 +0900

    small fix

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 'use strict';
 const assert = require('assert');
-const index = require('../');
+const pm = require('../');
 
 describe('#productMax()', () => {
   it('正確な計算ができる', () => {
-    assert.equal(index.productMax(1, 2, 3, 4), 8);
-    assert.equal(index.productMax(150, 200, 36, 428), 85600);
+    assert.equal(pm.productMax(1, 2, 3, 4), 8);
+    assert.equal(pm.productMax(150, 200, 36, 428), 85600);
   });
   it('負の値の計算ができる', () => {
-    assert.equal(index.productMax(-1, -2, 3, 4), -3);
-    assert.equal(index.productMax(-1, -2, -3, -4), 8);
+    assert.equal(pm.productMax(-1, -2, 3, 4), -3);
+    assert.equal(pm.productMax(-1, -2, -3, -4), 8);
   });
 });
